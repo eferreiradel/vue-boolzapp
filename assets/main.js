b9d1d7d892df8124e971303b78780db1d345e620
eferreiradel <cont.limaferreira.everton@gmail.com>
2023-05-15 12:14:16 +0200

    feat: lista utenti dinamica

--- assets/main.js
+++ assets/main.js
@@ -3,49 +3,49 @@ const { createApp } = Vue;
 createApp({
   data() {
     return {
+      active__element: 0,
       contacts: [
         {
           id: 0,
           name: "John",
           contact__photo: "./img/avatar_1.jpg",
-          messages: [
-            {
-              content: "",
-            },
-          ],
+          messages: [],
         },
         {
           id: 1,
           name: "Albert",
           contact__photo: "./img/avatar_2.jpg",
-          messages: [
-            {
-              content: "",
-            },
-          ],
+          messages: [],
+        },
+        {
+          id: 2,
+          name: "Kokori",
+          contact__photo: "./img/avatar_3.jpg",
+          messages: [],
         },
       ],
       // chatt
       message__commit: "",
       message__push: "",
-      message__history: [],
       //active chatt
       chat__active: "",
       chat__active__photo: "",
     };
   },
   methods: {
-    debug(event) {
-      const index = event.target.id;
+    returnIndex(event, i) {
+      index = event.target.id;
+      this.active__element = index;
       this.chat__active = this.contacts[index].name;
       this.chat__active__photo = this.contacts[index].contact__photo;
+      console.log(index);
     },
-    sendMsg(event) {
+    sendMsg(event, i) {
       this.message__push = this.message__commit;
-      this.message__history.push({
+      this.contacts[this.active__element].messages.push({
         content: this.message__push,
       });
-      console.log(this.message__history);
+      console.log(this.message__push);
     },
   },
 }).mount("#app");
